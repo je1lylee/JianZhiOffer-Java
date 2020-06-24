3d40fc67fd49d407cae14890d92f16606c14478e
liguodong0325 <908148781@qq.com>
2020-06-24 10:41:00 +0800

    完成08的第一种解法

---
+++ src/top/linxixixiangxin/offer08/solution01.java
@@ -0,0 +1,50 @@
+package top.linxixixiangxin.offer08;
+
+import java.util.Stack;
+
+public class solution01 {
+    public static void main(String[] args) {
+        CQueue08_01 cQueue = new CQueue08_01();
+        cQueue.appendTail(3);
+        System.out.println(cQueue.deleteHead());
+        System.out.println(cQueue.deleteHead());
+        System.out.println(cQueue.deleteHead());
+
+    }
+}
+
+class CQueue08_01 {
+    private Stack<Integer> main = new Stack<>();
+    private Stack<Integer> temp = new Stack<>();
+
+    public CQueue08_01() {
+
+    }
+
+    public void appendTail(int value) {
+        if (main.size() == 0) {
+            main.push(value);
+            return;
+        }
+        while (main.size() > 0) {
+            //清空main中的元素
+            temp.push(main.pop());
+        }
+        main.push(value);
+        while (temp.size() > 0) {
+            main.push(temp.pop());
+        }
+    }
+
+    public int deleteHead() {
+        if (main.size() == 0) return -1;
+        return main.pop();
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
