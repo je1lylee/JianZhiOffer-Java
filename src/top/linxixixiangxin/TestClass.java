84493dc3e44dfe2d9842e93b1ef3c51f494b23ae
liguodong0325 <908148781@qq.com>
2020-07-06 22:40:39 +0800

    编辑DP的四个特点

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -1,11 +1,22 @@
 package top.linxixixiangxin;
 
+import java.lang.reflect.Type;
 import java.util.UUID;
 import java.util.Vector;
 
 public class TestClass {
     public static void main(String[] args) {
-        Object object = new Object();
+        String para = "index".toUpperCase();
+        System.out.println(test.valueOf(para).getType());
     }
-
+}
+enum test{
+    INDEX(0);
+    private Integer type;
+    test(int type) {
+        this.type = type;
+    }
+    public Integer getType(){
+        return this.type;
+    }
 }
---
+++ src/top/linxixixiangxin/offer14/solution01.java
@@ -0,0 +1,13 @@
+package top.linxixixiangxin.offer14;
+
+public class solution01 {
+    public static void main(String[] args) {
+        System.out.println(new Solution14_01().cuttingRope(14));
+    }
+}
+class Solution14_01 {
+    public int cuttingRope(int n) {
+
+        return 0;
+    }
+}
