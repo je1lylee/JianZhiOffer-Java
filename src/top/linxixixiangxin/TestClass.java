717a6b69bf1c0a65cd245210024e02be5be5c169
liguodong0325 <908148781@qq.com>
2020-07-13 12:12:00 +0800

    完成19题和笔记

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -5,27 +5,7 @@ import java.util.*;
 
 public class TestClass {
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        int[] test = new Test().divingBoard(9,8436,28489);
-        System.out.print("耗时");
-        System.out.println(System.currentTimeMillis() - startTime);
-        for (int i = 0; i < test.length; i++) {
-            System.out.println(test[i]);
-        }
-    }
-}
-class Test{
-    public int[] divingBoard(int shorter, int longer, int k) {
-        if (k == 0){
-            return new int[0];
-        }
-        if(shorter == longer){
-            return new int[]{shorter * k};
-        }
-        int[] resultList = new int[k+1];
-        for(int i = 0;i <= k;i++){
-            resultList[i] = shorter * (k-i) + longer * i;
-        }
-        return resultList;
+        String test = "abcdefghijk";
+        System.out.println(test.substring(2));
     }
 }
