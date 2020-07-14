9f72341fb1a0772bf1d6a94a808d0a3d1c6e5b6f
liguodong0325 <908148781@qq.com>
2020-07-14 23:00:56 +0800

    细化注释

--- src/top/linxixixiangxin/offer20/solution01.java
+++ src/top/linxixixiangxin/offer20/solution01.java
@@ -6,14 +6,23 @@ public class solution01 {
     }
 }
 class Solution20_01{
+    /**
+     * 表示数值的字符串遵循模式A[.[B]][e|EC] 或者 .B[e|EC]
+     * A为数值的整数部分，B紧跟着小数点为数值的小数部分，C紧跟着e|E为数值的指数部分。
+     * 在小数里可能没有数值得到整数部分。
+     * 例如小数.123 等于0.123 因此A部分不是必须的，
+     * 如果一个数没有整数部分，那么他的小数部分不能为空
+     * @param s
+     * @return
+     */
     public boolean isNumber(String s) {
-        if ("959440.94f".equals(s)) return false;
-        try {
-            double test = Double.parseDouble(s);
-            System.out.println(test);
-            return true;
-        }catch (Exception e){
+        if(s == null || s.length() == 0){
             return false;
         }
+        return core(s,0);
+    }
+
+    public boolean core(String s,Integer index){
+
     }
 }
