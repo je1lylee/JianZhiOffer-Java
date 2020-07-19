c7011acccc1bef8656686881f19b842b6a343873
liguodong0325 <908148781@qq.com>
2020-07-19 18:40:45 +0800

    完成26题代码和笔记

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -5,10 +5,28 @@ import java.lang.reflect.Type;
 import java.util.*;
 
 public class TestClass {
-    public static void main(String[] args) {
-        Runtime runtime = Runtime.getRuntime();
-        System.out.println(System.getProperty("java.vm.specification.vendor"));
-
+    public static int aMethod(int i)throws Exception
+    {
+        try{
+            return i/10;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("exception in a aMethod");
+        }finally{
+            System.out.printf("finally");
+        }
+    }
+    public static void main(String[] args){
+        try
+        {
+            aMethod(0);
+        }
+        catch (Exception ex)
+        {
+            System.out.printf("exception in main");
+        }
+        System.out.printf("finished");
     }
     
 
---
+++ src/top/linxixixiangxin/offer26/solution01.java
@@ -0,0 +1,37 @@
+package top.linxixixiangxin.offer26;
+
+public class solution01 {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution26_01 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        /**
+         * 1.处理异常的输入，有一棵树为空就返回false
+         * 2.分别遍历根节点的两侧
+         */
+        return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
+    }
+    //先序遍历
+    boolean recur(TreeNode A, TreeNode B) {
+        //如果节点B为空，说明树B已匹配完成，越过了叶子节点，所以返回true
+        if(B == null) {
+            return true;
+        }
+        //A为空，说明越过过A的叶子节点，意味着匹配失败返回false
+        if(A == null || A.val != B.val) {
+            return false;
+        }
+        //遍历两棵树左侧，遍历两棵树右侧
+        return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+}
