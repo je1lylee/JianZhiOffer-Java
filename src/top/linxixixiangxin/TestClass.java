cb9d618fdc5daec2d345b99bf125bc72062378d7
liguodong0325 <908148781@qq.com>
2020-07-24 23:26:17 +0800

    31题框架

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -1,4 +1,6 @@
 package top.linxixixiangxin;
+import com.sun.corba.se.impl.orb.ParserTable;
+
 import java.lang.ref.SoftReference;
 import java.lang.reflect.Type;
 import java.net.MalformedURLException;
@@ -6,11 +8,11 @@ import java.net.URL;
 import java.util.*;
 
 public class TestClass {
-
+    private int a = 10;
+    int b = 20;
+    static int c = 1;
     public static void main(String args[]) {
-
     }
-
 
 
 }
---
+++ src/top/linxixixiangxin/offer31/solution01.java
@@ -0,0 +1,19 @@
+package top.linxixixiangxin.offer31;
+
+import java.util.Stack;
+
+public class solution01 {
+}
+class Solution31_01 {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        //处理特殊输入
+        if(pushed == null || popped == null || pushed.length != popped.length){
+            return false;
+        }
+        Stack<Integer> temp = new Stack<Integer>();
+        int indexOfPoped = 0;
+        int indexOfPushed = 0;
+
+
+    }
+}
