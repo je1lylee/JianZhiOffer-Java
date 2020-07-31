266903134be159f88607d9abd62b211b3d968e82
liguodong0325 <908148781@qq.com>
2020-07-31 23:39:00 +0800

    JUC test code

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -5,6 +5,7 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class TestClass {
+    //写 —> 读 阻塞写 非阻塞读
     public static void main(String args[]) {
         Account account = new Account("Mr.Zhang",0.0);
         double[] moneyData = new double[]{120.00,300.00,500,700,5000.0};
