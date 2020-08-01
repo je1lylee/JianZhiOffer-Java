40397f8ec9f5dce99b9cae3da98948066241b01d
liguodong0325 <908148781@qq.com>
2020-08-01 23:18:35 +0800

    JUC test code

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -5,24 +5,25 @@ import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class TestClass {
-    //写 —> 读 阻塞写 非阻塞读
+    public int test;
     public static void main(String args[]) {
-        Account account = new Account("Mr.Zhang",0.0);
-        double[] moneyData = new double[]{120.00,300.00,500,700,5000.0};
-        for (int x = 0;x <5;x++){
-            new Thread(() ->{
-                for (int y = 0;y<moneyData.length;y++){
-                    account.saveMoney(moneyData[y]);
-                }
-            }).start();
-        }
-        for (int x = 0;x<5;x++){
-            new Thread(() ->{
-                while (true){
-                    System.err.println(account.toString());
-                }
-            }).start();
-        }
+        int a;
+//        Account account = new Account("Mr.Zhang",0.0);
+//        double[] moneyData = new double[]{120.00,300.00,500,700,5000.0};
+//        for (int x = 0;x <5;x++){
+//            new Thread(() ->{
+//                for (int y = 0;y<moneyData.length;y++){
+//                    account.saveMoney(moneyData[y]);
+//                }
+//            }).start();
+//        }
+//        for (int x = 0;x<5;x++){
+//            new Thread(() ->{
+//                while (true){
+//                    System.err.println(account.toString());
+//                }
+//            }).start();
+//        }
     }
 
 
