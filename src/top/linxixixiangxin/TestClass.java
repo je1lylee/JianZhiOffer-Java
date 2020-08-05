3a65ad2dc5dc1e66614e0b57f02b1bd0bacf2619
liguodong0325 <908148781@qq.com>
2020-08-05 23:28:34 +0800

    40

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -7,61 +7,15 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class TestClass {
     public int test;
     public static void main(String[] args) {
-        int a;
-//        Account account = new Account("Mr.Zhang",0.0);
-//        double[] moneyData = new double[]{120.00,300.00,500,700,5000.0};
-//        for (int x = 0;x <5;x++){
-//            new Thread(() ->{
-//                for (int y = 0;y<moneyData.length;y++){
-//                    account.saveMoney(moneyData[y]);
-//                }
-//            }).start();
-//        }
-//        for (int x = 0;x<5;x++){
-//            new Thread(() ->{
-//                while (true){
-//                    System.err.println(account.toString());
-//                }
-//            }).start();
-//        }
+        Integer i01=59;
+        int i02=59;
+        Integer i03=Integer.valueOf(59);
+        Integer i04=new Integer(59);
+        System.out.println(i01==i02);
+        System.out.println(i01==i03);
+        System.out.println(i03==i04);
+        System.out.println(i02==i04);
     }
 
 
-}
-class Account{
-    private String name;
-    private double asset;
-    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-
-    public Account(String name, double asset) {
-        this.name = name;
-        this.asset = asset;
-    }
-    public void saveMoney(double money){
-        this.readWriteLock.writeLock().lock();
-        try{
-            this.asset += money;
-            TimeUnit.SECONDS.sleep(2);
-            System.out.println(Thread.currentThread().getName() + "is changing asset data:" + money
-            +"the current data is:"+this.asset);
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            this.readWriteLock.writeLock().unlock();
-        }
-    }
-    @Override
-    public String toString() {
-        this.readWriteLock.readLock().lock();
-        try{
-            return "Account info :" + Thread.currentThread().getName() +
-                    "Account Name :" + this.name +
-                    "Account Asset :" + this.asset;
-        }catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }finally {
-            this.readWriteLock.readLock().unlock();
-        }
-    }
 }
---
+++ src/top/linxixixiangxin/offer40/solution02.java
@@ -0,0 +1,15 @@
+package top.linxixixiangxin.offer40;
+
+import java.util.Arrays;
+
+public class solution02 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution40_02().getLeastNumbers(new int[]{3, 2, 1}, 2)));
+    }
+}
+class Solution40_02 {
+    //预留的
+    public int[] getLeastNumbers(int[] arr, int k) {
+        return new int[0];
+    }
+}
