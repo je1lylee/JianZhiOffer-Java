73a5f2ead654c6fc6e6ed632b6acb56eed059fba
liguodong0325 <908148781@qq.com>
2020-08-10 23:14:25 +0800

    netease

--- src/top/linxixixiangxin/TestClass.java
+++ src/top/linxixixiangxin/TestClass.java
@@ -1,47 +1,43 @@
 package top.linxixixiangxin;
 
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Stack;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.*;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println(fuc(1,5,new Integer[]{30,60,5,15,30}));
-    }
-//    public static int fuc(long[] array){
-//        int count = 0;
-//        if(array.length != 0){
-//            for (long i : array) {
-//                if(i<2){
-//                    continue;
-//                }else if(i == 2){
-//                    count++;
-//                }else{
-//                    count += i/2;
-//                }
-//            }
-//        }
-//        return count;
-//    }
-    public static int fuc(int T,int n,Integer[] a){
-        //以5举例，此时还剩下1 分配1
-        n -=  (n/2) * 2;
-        //刚好分配完 不用扔
-        if(n == 0) return 0;
-        Arrays.sort(a,Collections.reverseOrder());
-        int lastOK = 0;
-        int cur = 0;
-        for(int i = 1;i<a.length;i++){
-            if(cur == 0){
-                cur = a[i];
-            }else{
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        for(int k = 0;k<x;k++){
+            //原数组
+            String rawData = scanner.next();
+            //拼接后的字符串
+            StringBuilder string = new StringBuilder();
+            char tempA = '0',tempB = '0';boolean isDouble = false;
+            for(int i = 0;i<rawData.length();i++){
+                //已经检测到前面有两个一样的元素了
+                if(isDouble){
+                    if(rawData.charAt(i) == tempA){
+                        tempA = '0';
+                        isDouble = false;
+                        continue;
+                    }else if(rawData.charAt(i) != tempA && tempB == '0'){
+                        tempB = rawData.charAt(i);
+                    }else if (rawData.charAt(i) != tempA && rawData.charAt(i) == tempB){
+                        tempA = '0';
+                        tempB = '0';
+                        isDouble = false;
+                        continue;
+                    }
+
+                }else{
+                    string.append(rawData.charAt(i));
+                    if (tempA == '0' || tempA != string.charAt(i)){
+                        tempA = rawData.charAt(i);
+                    }else if(tempA == rawData.charAt(i)){
+                        isDouble = true;
+                    }
+                }
             }
+            System.out.println(string);
         }
-        return 0;
     }
 }
