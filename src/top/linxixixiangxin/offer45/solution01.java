2e279df33e337f8c89c5b6686af29fca586363aa
liguodong0325 <908148781@qq.com>
2020-08-30 20:36:31 +0800

    update

--- src/top/linxixixiangxin/offer45/solution01.java
+++ src/top/linxixixiangxin/offer45/solution01.java
@@ -7,7 +7,49 @@ public class solution01 {
 }
 class Solution45_01 {
     public String minNumber(int[] nums) {
-        
-        return "";
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strs[i] = String.valueOf(nums[i]);
+        //将int转成string 同时对字符串数组进行排序
+        fastSort(strs, 0, strs.length - 1);
+        //通过排序结果 构建数字
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+    }
+
+    /**
+     * 待排序数组 左侧指针 右侧指针
+     * @param strs
+     * @param l
+     * @param r
+     */
+    void fastSort(String[] strs, int l, int r) {
+        //左侧指针大于右侧指针 结束
+        if(l >= r) return;
+        int i = l, j = r;
+        String tmp = strs[i];
+        while(i < j) {
+            //campareTo >0 则前大于后 否则 后大于前 =0时两者相等
+            while((strs[j] + strs[l]).compareTo(strs[l] + strs[j]) >= 0 && i < j) {
+                //向左移动右侧指针
+                j--;
+            }
+            while((strs[i] + strs[l]).compareTo(strs[l] + strs[i]) <= 0 && i < j){
+                //想右移动左侧指针
+                i++;
+            }
+            //交换元素
+            tmp = strs[i];
+            strs[i] = strs[j];
+            strs[j] = tmp;
+        }
+        strs[i] = strs[l];
+        strs[l] = tmp;
+        //右侧元素左移
+        fastSort(strs, l, i - 1);
+        //左侧元素右移
+        fastSort(strs, i + 1, r);
     }
 }
--- src/top/linxixixiangxin/sort/Main.java
+++ src/top/linxixixiangxin/sort/Main.java
@@ -1,12 +1,13 @@
 package top.linxixixiangxin.sort;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int[] array = {49,60,40,66,79,89,22,80,81,98,19,56};
-        int[] res = selectionSort(array);
-        System.out.println(Arrays.toString(res));
+        quickSort(array);
+        System.out.println(Arrays.toString(array));
     }
 
     /**
@@ -79,12 +80,179 @@ public class Main {
      */
     public static int[] insertionSort(int[] array){
         //处理特殊输入
-        if (array.length == 0) return array;
-        for(int i = 0;i<array.length;i++){
-            for (int j = i;j>=0;j--){
-
+        if (array.length == 0)
+            return array;
+        //当前值
+        int current;
+        //外层遍历整个数组 同时默认第0个数字是排序好的
+        for (int i = 0; i < array.length - 1; i++) {
+            current = array[i + 1];
+            //前面一个数字的序号
+            int preIndex = i;
+            //如果数组没有遍历完 且 当前元素小于前一个元素
+            while (preIndex >= 0 && current < array[preIndex]) {
+                //继续向前遍历
+                array[preIndex + 1] = array[preIndex];
+                preIndex--;
             }
+            //数字已经到达了合适的位置
+            array[preIndex + 1] = current;
         }
         return array;
+    }
+    /**
+     * in-place sort
+     * 希尔排序是希尔（Donald Shell）于1959年提出的一种排序算法。希尔排序也是一种插入排序，它是简单插入排序经过改进之后的一个更高效的版本，也称为缩小增量排序，同时该算法是冲破O(n2）的第一批算法之一。它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序。
+     *
+     * 希尔排序是把记录按下表的一定增量分组，对每组使用直接插入排序算法排序；随着增量逐渐减少，每组包含的关键词越来越多，当增量减至1时，整个文件恰被分成一组，算法便终止。
+     *
+     * 4.1 算法描述
+     * 我们来看下希尔排序的基本步骤，在此我们选择增量gap=length/2，缩小增量继续以gap = gap/2的方式，这种增量选择我们可以用一个序列来表示，{n/2,(n/2)/2...1}，称为增量序列。希尔排序的增量序列的选择与证明是个数学难题，我们选择的这个增量序列是比较常用的，也是希尔建议的增量，称为希尔增量，但其实这个增量序列不是最优的。此处我们做示例使用希尔增量。
+     *
+     * 先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，具体算法描述：
+     *
+     * 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+     * 按增量序列个数k，对序列进行k 趟排序；
+     * 每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+     * 分组->排序->分组->排序 ······ 整个数列排序完成
+     */
+
+    public static int[] shellSort(int[] array){
+        int temp,gap = array.length/2;
+        //分组
+        while(gap > 0){
+            //在分组中进行插入排序
+            //遍历所有分组
+            for(int i = gap;i< array.length;i++){
+                temp = array[i];
+                int preIndex = i - gap;
+                //插入排序
+                while(preIndex >= 0 && array[preIndex] > temp){
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap]  = temp;
+            }
+            gap /=2;
+        }
+        return array;
+    }
+    /**
+     * 和选择排序一样，归并排序的性能不受输入数据的影响，但表现比选择排序好的多，
+     * 因为始终都是O(n log n）的时间复杂度。代价是需要额外的内存空间。
+     *
+     * 归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）
+     * 的一个非常典型的应用。归并排序是一种稳定的排序方法。将已有序的子序列合并，得到完全有序的序列；
+     * 即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
+     * 5.1 算法描述
+     * 把长度为n的输入序列分成两个长度为n/2的子序列；
+     * 对这两个子序列分别采用归并排序；
+     * 将两个排序好的子序列合并成一个最终的排序序列。
+     */
+    public static int[] mergeSort(int[] array){
+        //无效输入
+        if(array.length < 2) return array;
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array,0,mid);
+        int[] right = Arrays.copyOfRange(array,mid, array.length);
+        return merge(mergeSort(left),mergeSort(right));
+    }
+
+    /**
+     * 将两段排序好的数组结合成一个排序数组
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int[] merge(int[] left, int[] right) {
+        //合并完成后产生的数组
+        int[] result = new int[left.length + right.length];
+
+        //i j分别记录两个数组当前合并到的index
+        for (int index = 0, i = 0, j = 0; index < result.length; index++) {
+            if (i >= left.length)
+                result[index] = right[j++];
+            else if (j >= right.length)
+                result[index] = left[i++];
+            else if (left[i] > right[j])
+                result[index] = right[j++];
+            else
+                result[index] = left[i++];
+        }
+        return result;
+    }
+    /**
+     * 快速排序的基本思想：通过一趟排序将待排记录分隔成独立的两部分，其中一部分记录的关键字均比另一部分的关键字小，则可分别对这两部分记录继续进行排序，以达到整个序列有序。
+     *
+     * 6.1 算法描述
+     * 快速排序使用分治法来把一个串（list）分为两个子串（sub-lists）。具体算法描述如下：
+     *
+     * 从数列中挑出一个元素，称为 “基准”（pivot）；
+     * 重新排序数列，所有元素比基准值小的摆放在基准前面，所有元素比基准值大的摆在基准的后面（相同的数可以到任一边）。在这个分区退出之后，该基准就处于数列的中间位置。这个称为分区（partition）操作；
+     * 递归地（recursive）把小于基准值元素的子数列和大于基准值元素的子数列排序。
+     * 快速排序，使得整数数组 arr 有序
+     */
+    public static void quickSort(int[] arr) {
+        //处理特殊的输入数据 没有数组或只有一个数据
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        //提供数组 左侧和右侧指针
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * 快速排序，使得整数数组 arr 的 [L, R] 部分有序
+     */
+    public static void quickSort(int[] arr, int L, int R) {
+        if(L < R) {
+            // 把数组中随机的一个元素与最后一个元素交换，
+            // 这样以最后一个元素作为基准值实际上就是以数组中随机的一个元素作为基准值
+            swap(arr, new Random().nextInt(R - L + 1) + L, R);
+            //分割数组 将数组分割到只剩他一个元素，就代表那一部分排序完成了
+            int[] p = partition(arr, L, R);
+            //移动右侧指针
+            quickSort(arr, L, p[0] - 1);
+            //移动左侧指针
+            quickSort(arr, p[1] + 1, R);
+        }
+    }
+
+    /**
+     * 分区的过程，整数数组 arr 的[L, R]部分上，使得：
+     *   大于 arr[R] 的元素位于[L, R]部分的右边，但这部分数据不一定有序
+     *   小于 arr[R] 的元素位于[L, R]部分的左边，但这部分数据不一定有序
+     *   等于 arr[R] 的元素位于[L, R]部分的中间
+     *   返回等于部分的第一个元素的下标和最后一个下标组成的整数数组
+     */
+    public static int[] partition(int[] arr, int L, int R) {
+        //基准数
+        int basic = arr[R];
+        int less = L - 1;
+        int more = R + 1;
+        //左侧指针没有出界
+        while(L < more) {
+            //如果当前值小于基准值，则交换 同时左侧指针右移 交换完成后 less+1
+            if(arr[L] < basic) {
+                swap(arr, ++less, L++);
+            } else if (arr[L] > basic) {
+                swap(arr, --more, L);
+                //如果值相等 把左侧指针右移即可
+            } else {
+                L++;
+            }
+        }
+        //分割得到的边界
+        return new int[] { less + 1, more - 1 };
+
+    }
+
+    /*
+     * 交换数组 arr 中下标为 i 和下标为 j 位置的元素
+     */
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
